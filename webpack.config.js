25651e53c0e782d6eb19e6faf204ff5c3a244af8
number304 <jmpmateo3042013@gmail.com>
2021-03-15 18:58:54 -0500

    sass and copy plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -18,6 +20,13 @@ module.exports = {
             use: {
                 loader: 'babel-loader'
             }
+        },
+        {
+            test: /\.css|.styl$/i,
+            use: [MiniCssExtractPlugin.loader,
+                'css-loader',
+                'stylus-loader'
+            ]
         }
     ]},
     plugins: [
@@ -25,6 +34,15 @@ module.exports = {
             inject: true,
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'assets/images'),
+                    to: "assets/images"
+                }
+            ]
         })
     ]
 }
